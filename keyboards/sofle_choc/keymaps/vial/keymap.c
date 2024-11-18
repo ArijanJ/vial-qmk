26598010c97518f2beada0a14f27edcbdb3a861d
ArijanJ <arijanj@proton.me>
2024-11-18 22:35:46 +0100

    hold on lctl/esc other key

--- keyboards/sofle_choc/keymaps/vial/keymap.c
+++ keyboards/sofle_choc/keymaps/vial/keymap.c
@@ -61,6 +61,8 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
         case LT(7, KC_TAB): //if entering symbol layer
             // Immediately select the hold action when another key is pressed.
             return true;
+        case MT(MOD_LCTL, KC_ESC)
+            reture true;
         default:
             // Do not select the hold action when another key is pressed.
             return false;
